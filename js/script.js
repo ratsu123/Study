9a49a3c6759cec853ec3514901bc17a07e9c12ee
Ratmir <ratmir2002w@mail.ru>
2020-06-20 00:09:29 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// alert("hello world");
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt("как ваше имя", "");
+// answers[1] = prompt("как ваше фамилия", "");
+// answers[2] = prompt("сколько вам лет", "");
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+const category = "toys";
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'ivan';
+
+alert(`Hi,${user}`);
