77bc9a7ab671c8e7d1de11eef5962592e931dc30
Ratmir <ratmir2002w@mail.ru>
2020-06-20 19:56:21 +0600

    'cycles'

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@
 
 const numberOfFilms = +prompt("How many films you have watched?", "");
 
+if (numberOfFilms < 10) {
+    alert('Просмотренно довольно мало фильмов');
+} else if (numberOfFilms >= 10 && numberOfFilms <=30) {
+    alert('Вы классический зриетль');
+} else if (numberOfFilms > 30) {
+    alert('вы киноман');
+} else {
+    alert('произошла ошибка');
+}
+
 const personalMovieDB = {
     count : numberOfFilms,
     actors : {},
@@ -9,14 +19,26 @@ const personalMovieDB = {
     privat : false,
     movies : {}
 };
+for (let i = 0; i < 2; i++) {
+    const name1 = prompt("The last film you have watched?","");
+    const rate1 = prompt("Rate it from 1 to 10","0");
+    if (name1 != null && rate1 != null && name1 != '' && rate1 != '' && name1.length < 50) {
+        personalMovieDB.movies[name1] = rate1;
+        console.log('Done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+}
 
-let name1 = prompt("The last film you have watched?","");
-let rate1 = prompt("Rate it from 1 to 10","0");
-let name2 = prompt("The last film you have watched?","");
-let rate2 = prompt("Rate it from 1 to 10","0");
-personalMovieDB.movies[name1] = rate1;
-personalMovieDB.movies[name2] = rate2;
+
+
+
 console.log(personalMovieDB);
+
+
+
 
 
 
